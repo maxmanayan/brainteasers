2770d63f9fc76072967b1d34cede16c7f86c373a
Max Manayan <maximillian521@gmail.com>
2021-05-06 11:17:39 -0600

    init commit

---
+++ javascript/LC1119.js
@@ -0,0 +1,22 @@
+// Leetcode 1119 - remove vowels from a string
+
+// Input: s = "leetcodeisacommunityforcoders"
+// Output: "ltcdscmmntyfrcdrs"
+
+s = "leetcodeisacommunityforcoders"
+
+const removeVowels = (s) => {
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let noVowelsArr = []
+  let sArr = s.split("")
+  let newArr = sArr.filter( letter => {
+    if(letter !== 'a' && letter !== 'e' && letter !== 'i' && letter !== 'o' && letter !== 'u') {
+      return noVowelsArr.push(letter)
+    }
+  })
+  return newArr.join("")
+}
+
+
+
+console.log(removeVowels(s))
