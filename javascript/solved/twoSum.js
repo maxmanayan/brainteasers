272d49496737de0149a6a2c11484e3a3acfb8c3e
Max Manayan <maximillian521@gmail.com>
2021-07-07 06:24:08 -0700

    another solution to two sum

--- javascript/solved/twoSum.js
+++ javascript/solved/twoSum.js
@@ -1,17 +1,32 @@
-let nums = [3, 2, 4];
+let nums = [3, 2, 4, 9];
 let target = 6;
 
+// const twoSum = (nums, target) => {
+//   let sumArray = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     let res = target - nums[i];
+//     let resIndex = nums.indexOf(res);
+//     if (resIndex !== -1 && resIndex !== i) {
+//       sumArray = [i, resIndex];
+//       i += nums.length;
+//     }
+//   }
+//   return sumArray;
+// };
+
 const twoSum = (nums, target) => {
-  let sumArray = [];
+  let remainders = {};
+  let sumArr = [];
   for (let i = 0; i < nums.length; i++) {
-    let res = target - nums[i];
-    let resIndex = nums.indexOf(res);
-    if (resIndex !== -1 && resIndex !== i) {
-      sumArray = [i, resIndex];
+    let curNum = nums[i];
+    let res = target - curNum;
+    remainders[res] = 1;
+    if (curNum in remainders && curNum !== res) {
+      sumArr = [nums.indexOf(target - curNum), nums.indexOf(curNum)];
       i += nums.length;
     }
   }
-  return sumArray;
+  return sumArr;
 };
 
 console.log(twoSum(nums, target));
