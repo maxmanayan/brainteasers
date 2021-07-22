aa6e6d264cb13a90cd6fee64bc1d2d08a7ca3b9d
Max Manayan <maximillian521@gmail.com>
2021-07-22 12:42:18 -0600

    lc121 complete

---
+++ javascript/LC70.js
@@ -0,0 +1,16 @@
+// Leetcode#70 - Climbing Stairs
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+n = 3;
+
+const climbStairs = (n) => {};
